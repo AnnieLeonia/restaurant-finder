df7a10cb973128372f44607e11781166415ceb84
Annie Onnered <annie.onnered@tactel.se>
2023-07-26 11:36:24 +0200

    feat(components): add CategoryCard

---
+++ components/common/categoryCard/categoryCard.style.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet } from 'react-native';
+
+import { COLORS, SIZES } from '../../../constants';
+
+const styles = StyleSheet.create<any>({
+  container: {
+    backgroundColor: COLORS.primary,
+    flex: 1,
+    margin: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: SIZES.medium,
+  },
+  cardImageWrapper: {
+    height: 80,
+    width: 80,
+    backgroundColor: COLORS.secondary,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 100,
+  },
+  cardImage: {
+    width: '60%',
+    height: '60%',
+  },
+  cardText: {
+    fontSize: SIZES.large,
+  },
+});
+
+export default styles;
--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,7 @@
-import Nav from './common/nav/Nav';
-import SearchBar from './common/searchBar/SearchBar';
+import CategoryCard from './common/categoryCard/CategoryCard';
 import Header from './common/header/header';
-
+import Nav from './common/nav/Nav';
 import ScreenHeaderButton from './common/screenHeaderButton/ScreenHeaderButton';
+import SearchBar from './common/searchBar/SearchBar';
 
-export { Header, Nav, SearchBar, ScreenHeaderButton };
+export { CategoryCard, Header, Nav, ScreenHeaderButton, SearchBar };
